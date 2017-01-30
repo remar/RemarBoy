dbceb652be9309bb28f99e0110947070b05abe59
Andreas Remar <andreas.remar@gmail.com>
2017-01-30 14:11:29 +0100

    Remove premature optimization

--- src/CPU.h
+++ src/CPU.h
@@ -3,9 +3,6 @@
 
 #include "Memory.h"
 
-const int last_use_long = 0;
-const int last_use_short = 1;
-
 class CPU {
 private:
   const Memory& memory;
@@ -13,20 +10,10 @@ private:
   unsigned short PC;
   unsigned short SP;
 
-  unsigned short AF;
-  unsigned short BC;
-  unsigned short DE;
-  unsigned short HL;
-
   unsigned char A, F;
   unsigned char B, C;
   unsigned char D, E;
   unsigned char H, L;
-
-  int AF_last_use;
-  int BC_last_use;
-  int DE_last_use;
-  int HL_last_use;
 
 public:
   CPU(const Memory& memory);
