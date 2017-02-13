a9441519e98c0786d10a2ae919e4a11ad99f67b7
Andreas Remar <andreas.remar@gmail.com>
2017-02-13 18:52:35 +0100

    Extract printDisassembly

--- src/RemarBoy.cpp
+++ src/RemarBoy.cpp
@@ -9,16 +9,22 @@ Memory memory;
 Emulator emulator(&memory);
 Disassembler disasm(&memory);
 
+void printDisassembly() {
+  std::list<int> visited = memory.getVisited();
+  std::list<int>::iterator it = visited.begin();
+  for(;it != visited.end();it++) {
+    std::cout << "[" << std::setw(4) << std::setfill('0') << std::uppercase << std::hex << *it << "] " << disasm.disassemble(*it) << std::endl;
+  }
+}
+
 int main() {
   memory.insertCart("/home/andreas/Spel/roms/gb/Tetris.gb");
   emulator.printState();
   for(int i = 0;i < 100000;i++) {
     emulator.step();
   }
+
   emulator.printState();
-  std::list<int> visited = memory.getVisited();
-  std::list<int>::iterator it = visited.begin();
-  for(;it != visited.end();it++) {
-    std::cout << "[" << std::setw(4) << std::setfill('0') << std::uppercase << std::hex << *it << "] " << disasm.disassemble(*it) << std::endl;
-  }
+
+  printDisassembly();
 }
